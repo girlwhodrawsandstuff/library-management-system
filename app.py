da11ae64520cb55d019f2089c524b7e4cdec8227
Varsha Singh <varsha.singh0497@gmail.com>
2022-09-18 22:42:34 +0530

    feat: add routes for all categories

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask
 
+# books, members, transactions
+
 app = Flask(__name__)
 
 @app.route('/')
 def helloworld():
     return 'Hello, world!'
+
+@app.route('/members')
+def members():
+    return 'Members!'
+
+
+@app.route('/books')
+def books():
+    return 'Books!'
+
+@app.route('/transactions')
+def books():
+    return 'Transactions!'
