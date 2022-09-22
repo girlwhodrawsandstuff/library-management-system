bed0f40daf90460495888a419646689fda5b3281
Varsha Singh <varsha.singh0497@gmail.com>
2022-09-22 09:33:43 +0530

    feat: instantiate db with initial values

--- app.py
+++ app.py
@@ -2,27 +2,33 @@ from flask import Flask, render_template
 import requests
 from flask_sqlalchemy import SQLAlchemy
 
-# books, members, transactions
-
 app = Flask(__name__)
 
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 class Books(db.Model):
-    _id = db.Column("bookID", db.Integer, primary_key=True)
+    bookID = db.Column(db.String(250), primary_key=True)
     title = db.Column(db.String(250))
     authors = db.Column(db.String(250))
-    isbn = db.Column(db.Integer)
+    average_rating = db.Column(db.String(250))
+    isbn = db.Column(db.String(250))
+    isbn13 = db.Column(db.String(250))
+    language_code = db.Column(db.String(250))
+    num_pages = db.Column(db.String(250))
+    ratings_count = db.Column(db.String(250))
+    text_reviews_count = db.Column(db.String(250))
+    publication_date = db.Column(db.String(250))
     publisher = db.Column(db.String(250))
     stock = db.Column(db.Integer)
     borrowed_by = db.Column(db.String(200))  # TODO: needs to change unit
-    # TODO: add pages
 
 
 class Members(db.Model):
     _id = db.Column("id", db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     outstanding_debt = db.Column(db.String(100))
@@ -30,16 +36,23 @@ class Members(db.Model):
 
 
 url = 'https://frappe.io/api/method/frappe-library'
-
-
-def join_string(text):
-    return text
-
-
 response = requests.get(url)
 obj = response.json().get('message')
-mapped_obj = list(map(join_string, obj))
-books = Books(**{k: obj[k] for k in ('bookID', 'title', 'authors', 'isbn', 'publisher') if k in obj})
+
+
+def remove_dict_key_whitespaces(dictionary):
+    return {k.translate({32: None}): v for k, v in dictionary.items()}
+
+
+def add_to_database(list_of_dicts):
+    for d in list_of_dicts:
+        book_item = remove_dict_key_whitespaces(d)
+        book_entry = Books(**book_item)
+        db.session.add(book_entry)
+        db.session.commit()
+
+
+add_to_database(obj)
 
 
 @app.route('/')
@@ -49,7 +62,7 @@ def helloworld():
 
 @app.route('/books')
 def book():
-    return render_template("books.html", data=books)
+    return render_template("books.html")
 
 
 @app.route('/members')
